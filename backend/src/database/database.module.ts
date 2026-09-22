e5c8774e25345e448f99d464cd690d069c54780f
Sreeja Sarkar <sreejasarkar2017@gmail.com>
2026-09-22 13:23:41 +0530

    Database config changes

--- backend/src/database/database.module.ts
+++ backend/src/database/database.module.ts
@@ -1,15 +1,26 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
-@Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
+const databaseConfig = process.env.DATABASE_URL
+  ? {
+      url: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    }
+  : {
       host: process.env.DB_HOST,
       port: Number(process.env.DB_PORT),
       username: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
+    };
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      ...databaseConfig,
       autoLoadEntities: true,
       synchronize: true,
     }),
